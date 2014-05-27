1d62e79b97ecee68c762a97e414613f9efc4bd4d
xuejiangsnow <xuejiang1225@gmail.com>
2014-05-27 15:51:54 +0800

    privacy check

---
+++ src/Check/PrivacyCheck.java
@@ -0,0 +1,13 @@
+package Check;
+
+import Model.MarkovModel.MarkovModel;
+
+public class PrivacyCheck {
+	protected double d;
+	protected MarkovModel M;
+	
+	public PrivacyCheck(double i, MarkovModel m) {
+		d = i;
+		M = m;
+	}
+}
